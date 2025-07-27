8cdb75d88705a23aa8f05820d275a5b37079529b
jagtapK <jagtapkajal54@gmail.com>
2025-07-27 12:01:12 +0530

    Update Employee Details

--- src/main/java/com/employee/directory/employeeDirectorySystem/UpdateEmployee.java
+++ src/main/java/com/employee/directory/employeeDirectorySystem/UpdateEmployee.java
@@ -12,27 +12,26 @@ import employeeDirectory.Utility.employeeDirectoryUtility;
 public class UpdateEmployee {
 
 	public void UpdateEmpDetails() {
-	
-	SessionFactory factory = employeeDirectoryUtility.getSessionFactory();
-	
-	Session session = factory.openSession();
-	
-	Transaction transaction = session.beginTransaction();
-	
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter EmployeeId");
-	int Employee_id = sc.nextInt();
-	sc.nextLine();
-	
-	Employee_Data s1 = session.find(Employee_Data.class, Employee_id);
-	
-	System.out.println("Enter Updated Salary");
-	double empSalary = sc.nextDouble();
-	s1.setSalary(empSalary);
-	
-	transaction.commit();
-	
-	session.close();
-}
-}
 
+		SessionFactory factory = employeeDirectoryUtility.getSessionFactory();
+
+		Session session = factory.openSession();
+
+		Transaction transaction = session.beginTransaction();
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter EmployeeId");
+		int Employee_id = sc.nextInt();
+		sc.nextLine();
+
+		Employee_Data s1 = session.find(Employee_Data.class, Employee_id);
+
+		System.out.println("Enter Updated Salary");
+		double empSalary = sc.nextDouble();
+		s1.setSalary(empSalary);
+
+		transaction.commit();
+
+		session.close();
+	}
+}
